53161809ac176beff21dffac8121793bbc9d693d
Coen <coen.hacking@gmail.com>
2020-12-12 19:59:19 +0100

    Fixed error handling

--- backend/fetch.php
+++ backend/fetch.php
@@ -18,34 +18,33 @@ if(isset($_GET['author'])) {
 }
 
 # change for query
-$author = strtolower(str_replace(", ", "+", $author));
-$author = strtolower(str_replace(" ", "+", $author));
+$author = str_replace(", ", "+", $author);
+$author = str_replace(" ", "+", $author);
+$author = strtolower($author);
 
 # define cache url
 $cacheUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author.json";
+$xmlCacheUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author.xml";
+$commandUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author" . "_command.json";
 
 # check if cache available
 if (file_exists($cacheUrl)) {
     # retrieve from cache
     $jsonText = file_get_contents($cacheUrl);
 } else {
-    # split author name
-    $authorSplit = explode('+', $author);
-
     # create search query
     $query = "dementia[mesh]+AND+$author" . "[AU]";
 
     #assemble the esearch URL
-    $url = $base . "esearch.fcgi?db=$db&term=$query&usehistory=y";
+    $queryUrl = $base . "esearch.fcgi?db=$db&term=$query&usehistory=y";
     //    echo "$url<br/>";
 
     #post the esearch URL
-    $ch = curl_init($url); // such as http://example.com/example.xml
+    $ch = curl_init($queryUrl); // such as http://example.com/example.xml
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $output = curl_exec($ch);
     curl_close($ch);
-    //    echo "$output<br/>";
 
     try {
         # convert into xml structure
@@ -55,27 +54,33 @@ if (file_exists($cacheUrl)) {
         $web = $output->{'WebEnv'};
         $key = $output->{'QueryKey'};
 
-        ### include this code for ESearch-ESummary
-        #assemble the esummary URL
-        $url = $base . "esummary.fcgi?db=$db&query_key=$key&WebEnv=$web";
-
-        #post the esummary URL
-        $docsums = file_get_contents($url);
-        //echo "$docsums";
-
         ### include this code for ESearch-EFetch
         #assemble the efetch URL
         $url = $base . "efetch.fcgi?db=$db&query_key=$key&WebEnv=$web";
         $url .= "&rettype=abstract&retmode=xml";
 
-        #post the efetch URL
-        $data = file_get_contents($url);
+        # post the eFetch URL
+        $ch = curl_init($url); // such as http://example.com/example.xml
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        # convert into json
         $xml = simplexml_load_string($data);
         $jsonText = json_encode($xml);
 
-        if ($jsonText != null) {
+        if ($jsonText != null && !strpos($data, 'error')) {
             $newFile = fopen($cacheUrl, "w");
             fwrite($newFile, $jsonText);
+            fclose($newFile);
+        } else {
+            $newFile = fopen($xmlCacheUrl, "w");
+            fwrite($newFile, $data);
+            fclose($newFile);
+            
+            $newFile = fopen($commandUrl, "w");
+            fwrite($newFile, '["' . $queryUrl . '", "' . $url . '"]');
             fclose($newFile);
         }
     } catch (Exception $e) {
