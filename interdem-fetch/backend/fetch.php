d3847408472ec13d47a58eb50a404503b0481e6f
Coen <coen.hacking@gmail.com>
2021-01-18 11:31:26 +0100

    Many improvements

--- interdem-fetch/backend/fetch.php
+++ interdem-fetch/backend/fetch.php
@@ -11,7 +11,7 @@ $base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/';
 
 # add authors to query
 if(isset($_GET['author'])) {
-    $author = $_GET['author'];
+    $author = urldecode($_GET['author']);
 } else {
     # for testing
     $author = "Aguirre, Elisa";
@@ -27,67 +27,76 @@ $cacheUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author.json";
 $xmlCacheUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author.xml";
 $commandUrl = $_SERVER['DOCUMENT_ROOT']."/data/articles/$author" . "_command.json";
 
+# load cache
+$jsonText = '';
+if (file_exists($cacheUrl))
+  $jsonText = file_get_contents($cacheUrl);
+
 # check if cache available
-if (file_exists($cacheUrl) &&
-  time() - filemtime($cacheUrl) < 24 * 3600) { # if less then 24 hours old
-    # retrieve from cache
-    $jsonText = file_get_contents($cacheUrl);
-} else {
-    # create search query
-    $query = "dementia[mesh]+AND+$author" . "[AU]";
+if (!file_exists($cacheUrl) ||
+  time() - filemtime($cacheUrl) > 24 * 3600) {
 
-    #assemble the esearch URL
-    $queryUrl = $base . "esearch.fcgi?db=$db&term=$query&usehistory=y";
-    //    echo "$url<br/>";
+  # create search query
+  $query = "dementia[mesh]+AND+$author" . "[AU]";
 
-    #post the esearch URL
-    $ch = curl_init($queryUrl); // such as http://example.com/example.xml
+  #assemble the esearch URL
+  $queryUrl = $base . "esearch.fcgi?db=$db&term=$query&usehistory=y";
+  //    echo "$url<br/>";
+
+  #post the esearch URL
+  $ch = curl_init($queryUrl); // such as http://example.com/example.xml
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  $output = curl_exec($ch);
+  curl_close($ch);
+
+  try {
+    # convert into xml structure
+    $output = new SimpleXMLElement($output);
+
+    #parse WebEnv and QueryKey
+    $web = $output->{'WebEnv'};
+    $key = $output->{'QueryKey'};
+
+    ### include this code for ESearch-EFetch
+    #assemble the efetch URL
+    $url = $base . "efetch.fcgi?db=$db&query_key=$key&WebEnv=$web";
+    $url .= "&rettype=abstract&retmode=xml";
+
+    # post the eFetch URL
+    $ch = curl_init($url); // such as http://example.com/example.xml
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    $output = curl_exec($ch);
+    $data = curl_exec($ch);
     curl_close($ch);
 
-    try {
-        # convert into xml structure
-        $output = new SimpleXMLElement($output);
+    # convert into json
+    $xml = simplexml_load_string($data);
+    $jsonTextTmp = json_encode($xml);
 
-        #parse WebEnv and QueryKey
-        $web = $output->{'WebEnv'};
-        $key = $output->{'QueryKey'};
+    if ($jsonTextTmp != null) {
+      $newFile = fopen($cacheUrl, "w");
+      fwrite($newFile, $jsonTextTmp);
+      fclose($newFile);
 
-        ### include this code for ESearch-EFetch
-        #assemble the efetch URL
-        $url = $base . "efetch.fcgi?db=$db&query_key=$key&WebEnv=$web";
-        $url .= "&rettype=abstract&retmode=xml";
+      $jsonText = $jsonTextTmp;
+    } else {
+      $newFile = fopen($xmlCacheUrl, "w");
+      fwrite($newFile, $data);
+      fclose($newFile);
 
-        # post the eFetch URL
-        $ch = curl_init($url); // such as http://example.com/example.xml
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
+      $newFile = fopen($commandUrl, "w");
+      fwrite($newFile, '["' . $queryUrl . '", "' . $url . '"]');
+      fclose($newFile);
 
-        # convert into json
-        $xml = simplexml_load_string($data);
-        $jsonText = json_encode($xml);
-
-        if ($jsonText != null && !strpos($data, 'error')) {
-            $newFile = fopen($cacheUrl, "w");
-            fwrite($newFile, $jsonText);
-            fclose($newFile);
-        } else {
-            $newFile = fopen($xmlCacheUrl, "w");
-            fwrite($newFile, $data);
-            fclose($newFile);
-
-            $newFile = fopen($commandUrl, "w");
-            fwrite($newFile, '["' . $queryUrl . '", "' . $url . '"]');
-            fclose($newFile);
-        }
-    } catch (Exception $e) {
-        // no articles founds
-        $jsonText = null;
+      throw new Exception('data was null or faulty');
     }
+  } catch (Exception $e) {
+    // no articles founds
+    if ($jsonText == null) {
+      echo $e->getMessage();
+    }
+  }
 }
 
 echo $jsonText;
--- interdem-fetch/src/interdem-fetch.ts
+++ interdem-fetch/src/interdem-fetch.ts
@@ -11,16 +11,19 @@ export class InterdemFetch extends LitElement {
   @property({type: Array}) papers?: any[] = [
     {
       id: 0,
+      show: true,
       name: 'Diagnosis and development of symptoms',
       keywords: [
-        { name: 'diagnosis', weight: 1 },
-        { name: 'diagnostic', weight: 1 },
-        { name: 'prognosis', weight: 1 },
-        { name: 'symptoms', weight: 1 },
-        { name: 'validation', weight: 1 }
+        { name: 'diagnosis', weight: 10 },
+        { name: 'diagnostic', weight: 10 },
+        { name: 'prognosis', weight: 10 },
+        { name: 'symptoms', weight: 10 },
+        { name: 'validation', weight: 10 },
+        { name: 'validate', weight: 10 }
       ]
     }, {
       id: 1,
+      show: true,
       name: 'Health Economics',
       keywords: [
         { name: 'economic', weight: 100 },
@@ -28,6 +31,7 @@ export class InterdemFetch extends LitElement {
       ]
     }, {
       id: 2,
+      show: true,
       name: 'Organization of dementia care',
       keywords: [
         { name: 'organization', weight: 10 },
@@ -35,16 +39,20 @@ export class InterdemFetch extends LitElement {
       ]
     }, {
       id: 3,
+      show: true,
       name: 'Persons need, quality of life and social/ethical issues',
       keywords: [
         { name: 'social', weight: 1 },
         { name: 'ethical', weight: 1 },
         { name: 'ethics', weight: 1 },
         { name: 'persons need', weight: 1 },
-        { name: 'quality of life', weight: 1 }
+        { name: 'quality of life', weight: 1 },
+        { name: 'decision-making', weight: 1 },
+        { name: 'end-of-life', weight: 1 }
       ]
     }, {
       id: 4,
+      show: true,
       name: 'Psychosocial interventions for persons with dementia',
       keywords: [
         { name: 'intervention', weight: 1 },
@@ -53,6 +61,7 @@ export class InterdemFetch extends LitElement {
       ]
     }, {
       id: 5,
+      show: true,
       name: 'Psychosocial interventions for caregivers',
       keywords: [
         { name: 'intervention', weight: 1 },
@@ -62,16 +71,30 @@ export class InterdemFetch extends LitElement {
       ]
     }, {
       id: 7,
-      name: 'Biomedical Science',
+      show: false,
+      name: 'Neuroscience',
       keywords: [
-        { name: 'synaptic', weight: 1 },
-        { name: 'exosomes', weight: 1 },
-        { name: 'cerebrospinal', weight: 1 },
-        { name: 'fluid', weight: 1 },
-        { name: 'progranulin', weight: 1 },
-        { name: 'ultrasound', weight: 1 },
+        { name: 'Amyloid', weight: 100 },
+        { name: 'PET', weight: 10 },
+        { name: 'neurofilament', weight: 100 },
+        { name: 'synaptic', weight: 10 },
+        { name: 'apolipoprotein', weight: 100 },
+        { name: 'butyrylcholinesterase', weight: 100 },
+        { name: 'frontotemporal', weight: 10 },
+        { name: 'vascular dementia', weight: 10 },
+        { name: 'genetics', weight: 10 },
+        { name: 'brain function', weight: 1 },
+        { name: 'lewy bodies', weight: 10 },
+        { name: 'exosomes', weight: 10 },
+        { name: 'cerebrospinal', weight: 10 },
+        { name: 'fluid', weight: 10 },
+        { name: 'progranulin', weight: 10 },
+        { name: 'ultrasound', weight: 10 },
         { name: 'drug use', weight: 1 },
-        { name: 'drug', weight: 1 }
+        { name: 'drug', weight: 1 },
+        { name: 'acetyltransferase', weight: 100 },
+        { name: 'receptors', weight: 10 },
+        { name: 'genotype', weight: 1 }
       ]
     }
   ];
@@ -84,7 +107,7 @@ export class InterdemFetch extends LitElement {
     }
 
     .page-width {
-      width: 100%;
+      width: 95%;
       max-width: 1000px;
       margin: 0 auto;
     }
@@ -103,7 +126,7 @@ export class InterdemFetch extends LitElement {
     return html`
       <div class="page-width">
         <h1>Articles</h1>
-        ${this.papers ? repeat(this.papers, category => html`
+        ${this.papers ? repeat(this.papers, category => category.show ? html`
           <h2>${category.name}</h2>
           ${category?.years ? repeat(category.years, (yearGroup: any) => html`
             <vaadin-details>
@@ -116,11 +139,12 @@ export class InterdemFetch extends LitElement {
                 <a href="https://pubmed.ncbi.nlm.nih.gov/${article.MedlineCitation?.PMID}/">
                   ${article?.MedlineCitation?.Article?.ArticleTitle}
                 </a>
+                ${article?.MedlineCitation?.Article?.Journal?.Title}
               </p>
             `) : ''}
             </vaadin-details>
           `) : ''}
-        `) : ''}
+        ` : '') : ''}
         <br/>
         <br/>
       </div>
@@ -134,12 +158,17 @@ export class InterdemFetch extends LitElement {
       this.authors = list;
 
       list.forEach(async (author: { name: string, data?: any }) => {
-        await fetch('/backend/fetch.php?author=' + author.name).then(res => res.json()).then((data: any) => {
+        const url = '/backend/fetch.php?author=' + encodeURIComponent(author.name);
+
+        // slow down requests at a maximum of 3 per second to avoid PubMed blocking
+        // await new Promise(r => setTimeout(r, Math.random() * list.length * 1000 / 3));
+
+        await fetch(url).then(res => res.json()).then((data: any) => {
           author.data = data;
           this.addAuthorData(author);
           this.requestUpdate();
         }).catch(() => {
-          console.log(author.name + ' is not available');
+          console.log(author.name + ' is not available (' + url + ')');
         });
       })
     });
@@ -149,7 +178,12 @@ export class InterdemFetch extends LitElement {
     if (author.data && author.data['PubmedArticle'] && Array.isArray(author.data['PubmedArticle'])) {
       author.data['PubmedArticle'].forEach(paper => {
         if (paper['MedlineCitation'] && paper['MedlineCitation']['Article']) {
-          let year = paper['MedlineCitation']['DateRevised']['Year'];
+          let year = paper['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']['Year'];
+          if (year == null || year == '')
+            year = paper['MedlineCitation']['DateCompleted']['Year'];
+          if (year == null || year == '')
+            return; // the year is not known, don't view this one
+
           let title = paper['MedlineCitation']['Article']['ArticleTitle'];
           let paperAbstract = '';
 
@@ -235,7 +269,7 @@ export class InterdemFetch extends LitElement {
     let numMatches = 0;
 
     keywords.forEach((x: { name: string, weight: number}) => {
-      let regex = new RegExp(x.name, 'g');
+      let regex = new RegExp(x.name.toLowerCase(), 'g');
       let matches = text.match(regex);
       if (matches)
         numMatches += matches.length * x.weight;
